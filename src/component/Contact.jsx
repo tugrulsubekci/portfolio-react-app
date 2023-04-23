e5aaac820419c55128b5548a76265b6dc3cb551c
Tuğrul Sübekci <104980354+tugrulsubekci@users.noreply.github.com>
2023-04-23 17:22:00 +0300

    contact form fixes

--- src/component/Contact.jsx
+++ src/component/Contact.jsx
@@ -1,7 +1,32 @@
-import React  from "react";
+import React, {useRef}  from "react";
 import "../styles/styles.css";
+import emailjs from '@emailjs/browser';
 
 function Contact() {
+    const form = useRef();
+
+    const [isVisible, setVisibility] = React.useState(false);
+
+    const [isDisabled, setDisabled] = React.useState(false);
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        setDisabled(true);
+        setInterval(() => {
+            setDisabled(false);
+        }, 5000);
+        
+        emailjs.sendForm('service_b1xvflc', 'template_q2wqqaz', form.current, 'jPvrehj5YEWwe7AAk')
+        .then((result) => {
+            console.log(result.text);
+            if(result.text === "OK")
+                setVisibility(true);
+        }, (error) => {
+            console.log(error.text);
+        });
+    };
+
     return (
     <div id="contact">
         <div className="container">
@@ -18,11 +43,12 @@ function Contact() {
                     <a href="images/Tugrul-Subekci-Resume.pdf" download className="btn btn2">Download CV</a>
                 </div>
                 <div className="contact-right">
-                    <form name="submit-to-google-sheet">
-                        <input type="text" name="Name" placeholder="Your Name" required />
-                        <input type="email" name="Email" placeholder="Your Email" required />
-                        <textarea name="Message" rows="6" placeholder="Your Message"></textarea>
-                        <button type="submit" className="btn btn2">Submit</button>
+                    <form ref={form} onSubmit={sendEmail}>
+                        <input type="text" name="user_name" placeholder="Your Name" required />
+                        <input type="email" name="user_email" placeholder="Your Email" required />
+                        <textarea name="message" rows="6" placeholder="Your Message"></textarea>
+                        <p style={{visibility: (isVisible ? "visible" : "hidden")}} >Your message has been sent</p>
+                        <button type="submit" className={"btn btn2" + (isDisabled ? " disabledBtn" : "")} disabled={isDisabled}>Submit</button>
                     </form>
                     <span id="msg"></span>
                 </div>
