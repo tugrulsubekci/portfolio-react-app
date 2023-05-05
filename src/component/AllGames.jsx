048e8377b4f11b19401de89826b6f0488693d577
Tuğrul Sübekci <104980354+tugrulsubekci@users.noreply.github.com>
2023-05-06 01:20:29 +0300

    all games section completed

--- src/component/AllGames.jsx
+++ src/component/AllGames.jsx
@@ -1,21 +1,83 @@
 import React  from "react";
 import { useNavigate } from "react-router-dom";
+import "../styles/allGames.css";
 
 function AllGames() {
     const navigate = useNavigate()
 
     return (
-        <div className="grid-container">
-            <div className="grid-item">
-                <button onClick={() => navigate("rapidRender")}>
-                    <img className="game-icon" src={`${process.env.PUBLIC_URL}/logos/rapid-render-logo.png`}></img>
-                </button>
-                <h2>Rapid render</h2>
+        <div class="wrapper-grid-allgames root-allgames">
+            <div class="container-allgames">
+                <div class='banner-img-allgames'></div>
+                <img src={`${process.env.PUBLIC_URL}/logos/rapid-render-logo.png`} alt='profile image' class="profile-img-allgames" />
+                <h1 class="name-allgames">Rapid Render</h1>
+                <p class="description-allgames">An interior design application</p>
+                <button class='btn-allgames'>Show details</button>
             </div>
-            <div className="grid-item"></div>
-            <div className="grid-item"></div>
-            <div className="grid-item"></div>
-            <div className="grid-item"></div>
+
+            <div class="container-allgames">
+                <div class='banner-img-allgames'></div>
+                <img src={`${process.env.PUBLIC_URL}/logos/connect-them-all-logo.png`} alt='profile image' class="profile-img-allgames" />
+                <h1 class="name-allgames">Connect Them All</h1>
+                <p class="description-allgames">Puzzle</p>
+                <button class='btn-allgames'>Show details</button>
+            </div>
+
+            <div class="container-allgames">
+                <div class='banner-img-allgames'></div>
+                <img src={`${process.env.PUBLIC_URL}/logos/soccer-ball-run-logo.png`} alt='profile image' class="profile-img-allgames" />
+                <h1 class="name-allgames">Soccer Ball Run</h1>
+                <p class="description-allgames">Platformer</p>
+                <button class='btn-allgames'>Show details</button>
+            </div>
+
+            <div class="container-allgames">
+                <div class='banner-img-allgames'></div>
+                <img src={`${process.env.PUBLIC_URL}/logos/tetris-logo.png`} alt='profile image' class="profile-img-allgames" />
+                <h1 class="name-allgames">Tetris</h1>
+                <p class="description-allgames">Puzzle</p>
+                <button class='btn-allgames'>Show details</button>
+            </div>
+
+            <div class="container-allgames">
+                <div class='banner-img-allgames'></div>
+                <img src={`${process.env.PUBLIC_URL}/logos/pacman-logo.png`} alt='profile image' class="profile-img-allgames" />
+                <p class="name-allgames">Pacman</p>
+                <p class="description-allgames">Action</p>
+                <button class='btn-allgames'>Show details</button>
+            </div>
+
+            <div class="container-allgames">
+                <div class='banner-img-allgames'></div>
+                <img src={`${process.env.PUBLIC_URL}/logos/snake-logo.png`} alt='profile image' class="profile-img-allgames" />
+                <p class="name-allgames">Snake</p>
+                <p class="description-allgames">Hyper Casual</p>
+                <button class='btn-allgames'>Show details</button>
+            </div>
+
+            <div class="container-allgames">
+                <div class='banner-img-allgames'></div>
+                <img src={`${process.env.PUBLIC_URL}/logos/helix-jump-logo.png`} alt='profile image' class="profile-img-allgames" />
+                <p class="name-allgames">Helix Jump</p>
+                <p class="description-allgames">Hyper Casual</p>
+                <button class='btn-allgames'>Show details</button>
+            </div>
+
+            <div class="container-allgames">
+                <div class='banner-img-allgames'></div>
+                <img src={`${process.env.PUBLIC_URL}/logos/chemistry-lab-logo.png`} alt='profile image' class="profile-img-allgames" />
+                <p class="name-allgames">Chemistry Lab</p>
+                <p class="description-allgames">Educational</p>
+                <button class='btn-allgames'>Show details</button>
+            </div>
+
+            <div class="container-allgames">
+                <div class='banner-img-allgames'></div>
+                <img src={`${process.env.PUBLIC_URL}/logos/my-bird-logo.png`} alt='profile image' class="profile-img-allgames" />
+                <p class="name-allgames">My Bird</p>
+                <p class="description-allgames">Hyper Casual</p>
+                <button class='btn-allgames'>Show details</button>
+            </div>
         </div>
     );
   }
