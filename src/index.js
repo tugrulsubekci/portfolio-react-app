fb2bc82cf378fbbacb5c13d09cc44b35d52b1297
Tuğrul Sübekci <104980354+tugrulsubekci@users.noreply.github.com>
2024-01-08 00:41:31 +0300

    ga4 initiliaze

--- src/index.js
+++ src/index.js
@@ -2,8 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './component/App';
 import { BrowserRouter } from "react-router-dom";
+import ReactGA from "react-ga4";
 
+ReactGA.initialize("G-02SNMFSHN6");
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
     <BrowserRouter>
